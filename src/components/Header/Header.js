edfa0e84eb1a672c82d5ee5fcffc8abd178f3094
Francisco Kahil <francisco.kahil@luizalabs.com>
2020-09-18 21:26:57 -0300

    Add Home Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import { ReactComponent as Logo } from '../../img/logo.svg';
+
 import './Header.css';
 
 function Header() {
   return (
     <div className="Header">
-        Marvel Search Heroes
+      <Logo title="Marvel - Search heroes" />
+      <h2 className="Header-title">EXPLORE O UNIVERSO</h2>
+      <h3 className="Header-subtitle">
+        Mergulhe no domínio deslumbrante de todos os personagens clássicos que
+        você ama - e aqueles que você descobrirá em breve!
+      </h3>
     </div>
   );
 }
