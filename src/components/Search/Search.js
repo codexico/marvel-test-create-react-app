9fb8f892023ea4ec39c1f36b09bb210e1fa543d2
Francisco Kahil <francisco.kahil@luizalabs.com>
2020-09-19 14:46:28 -0300

    Add Search form

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import { ReactComponent as SearchIcon } from '../../img/search.svg';
+
 import './Search.css';
 
 function Search() {
   return (
     <div className="Search">
-        <input className="Search-input" />
+      <form className="Search-form">
+        <div className="Search-inputGroup">
+          <label htmlFor="" className="Search-label">
+            <button className="Search-inputButton">
+              <SearchIcon className="Search-inputIcon" />
+            </button>
+            <input className="Search-input" placeholder="Procure por heróis" />
+          </label>
+        </div>
+      </form>
     </div>
   );
 }
