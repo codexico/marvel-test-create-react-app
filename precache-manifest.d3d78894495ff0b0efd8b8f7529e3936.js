14682bb6539c0e2274da8b7634d1946baf93f5b4
Francisco Kahil <francisco.kahil@luizalabs.com>
2020-09-20 17:48:47 -0300

    Updates

---
+++ precache-manifest.d3d78894495ff0b0efd8b8f7529e3936.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab1996194b11c2188bc4d7ea45076d6",
+    "url": "/marvel/index.html"
+  },
+  {
+    "revision": "dccdea882649e63e210b",
+    "url": "/marvel/static/css/main.4e5ccb2c.chunk.css"
+  },
+  {
+    "revision": "cc31dc3a9ace5416b1dd",
+    "url": "/marvel/static/js/2.aafdf482.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marvel/static/js/2.aafdf482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dccdea882649e63e210b",
+    "url": "/marvel/static/js/main.375b043c.chunk.js"
+  },
+  {
+    "revision": "fe4cffbd319b1a4279f9",
+    "url": "/marvel/static/js/runtime-main.0aa03bd5.js"
+  },
+  {
+    "revision": "8352c5c3de31a1cdd01ad88e066ed874",
+    "url": "/marvel/static/media/fav.8352c5c3.svg"
+  },
+  {
+    "revision": "61bd6eb829e1e3d6b7729042dfc84f62",
+    "url": "/marvel/static/media/heart.61bd6eb8.svg"
+  },
+  {
+    "revision": "84b6ff57b6ef5716b609813443862af5",
+    "url": "/marvel/static/media/logo.84b6ff57.svg"
+  },
+  {
+    "revision": "aeefa6375ff61674da4b9c8cdb51faca",
+    "url": "/marvel/static/media/search.aeefa637.svg"
+  }
+]);
